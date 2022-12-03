33812b91f1e1f3b0f935a021424b8080357384dd
Andriy Yendrizhyewskiy <for.a.kvadrat@gmail.com>
2022-12-03 21:33:56 +0200

    Add keyboard listener

---
+++ untitled/src/main/java/dvsn/gl/Main.java
@@ -0,0 +1,19 @@
+package dvsn.gl;
+
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException ex) {
+            System.err.println("There was a problem registering the native hook.");
+            System.err.println(ex.getMessage());
+
+            System.exit(1);
+        }
+
+        GlobalScreen.addNativeKeyListener(new GlobalKeyListener());
+    }
+}
